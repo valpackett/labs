9fa0e8e8401e1579a68d28ce8c49f8c1bc224c8d
myfreeweb <floatboth@me.com>
2012-03-21 18:45:27 +0400

    redis var update

--- www/.aspen/config.py
+++ www/.aspen/config.py
@@ -2,9 +2,9 @@ import os
 import redis
 import urlparse
 
-if os.environ.has_key('REDIS_TO_GO'):
+if os.environ.has_key('REDISTOGO_URL'):
     urlparse.uses_netloc.append('redis')
-    url = urlparse.urlparse(os.environ['REDIS_TO_GO'])
+    url = urlparse.urlparse(os.environ['REDISTOGO_URL'])
     REDIS = redis.Redis(host=url.hostname, port=url.port, db=0,
             password=url.password)
 else:
